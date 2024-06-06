a554475135fc1ab36e3a3bd5ad7379b0ddd7c256
filgueira5 <dev.filgueiras@gmail.com>
2024-06-06 09:48:35 -0400

    Animation Update

---
+++ script.js
@@ -0,0 +1,26 @@
+window.show = ScrollReveal({reset:true})
+
+show.reveal('#my-name',
+    {
+        duration: 3500,
+        distance: '90px',
+        origin: 'top'
+    }
+)
+
+
+show.reveal('#main-title',
+    {
+        duration: 2500,
+        distance: '90px',
+        origin: 'top'
+    }
+)
+
+show.reveal('#main-subtitle',
+    {
+        duration: 1500,
+        distance: '90px',
+        origin: 'top'
+    }
+)
